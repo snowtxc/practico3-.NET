59bc418b8551c232ed379c4f3feec179396c4e65
cbauza <cristian.bauza.silva@gmail.com>
2023-09-06 18:12:09 -0300

    Add EF DAL

--- DataAccessLayer/DALs/DAL_Personas_ADONET.cs
+++ DataAccessLayer/DALs/DAL_Personas_ADONET.cs
@@ -11,109 +11,29 @@ namespace DataAccessLayer.DALs
 {
     public class DAL_Personas_ADONET : IDAL_Personas
     {
-        private string _connectionString = "Server=localhost,1433;Database=Practico2;User Id=sa;Password=Abc*123!;Encrypt=False;";
+        public void Delete(string documento)
+        {
+            throw new NotImplementedException();
+        }
 
         public List<Persona> Get()
         {
-            // Instanciar la lista de personas a devolver
-            List<Persona> personas = new List<Persona>();
-            
-            // Abrir la conexión a la base de datos
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("SELECT * FROM Personas", connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Persona persona = new Persona();
-
-                            persona.Documento = reader["Documento"].ToString();
-                            persona.Nombre = reader["Nombres"].ToString();
-
-                            personas.Add(persona);
-                        }
-                    }
-                }
-            }
-
-            return personas;
+            throw new NotImplementedException();
         }
 
         public Persona Get(string documento)
         {
-            Persona persona;
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("SELECT * FROM Personas WHERE Documento = @documento", connection))
-                {
-                    command.Parameters.AddWithValue("documento", documento);
-
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        reader.Read();
-                        persona = new Persona();
-
-                        persona.Documento = reader["Documento"].ToString();
-                        persona.Nombre = reader["Nombres"].ToString();
-                    }
-                }
-            }
-
-            return persona;
+            throw new NotImplementedException();
         }
 
         public void Insert(Persona persona)
         {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("INSERT INTO Personas (Documento, Nombres) VALUES (@documento, @nombres)", connection))
-                {
-                    command.Parameters.AddWithValue("@documento", persona.Documento);
-                    command.Parameters.AddWithValue("@nombres", persona.Nombre);
-
-                    command.ExecuteNonQuery();
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public void Update(Persona persona)
         {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("UPDATE Personas SET Nombres = @nombre WHERE Documento = @documento", connection))
-                {
-                    command.Parameters.AddWithValue("@documento", persona.Documento);
-                    command.Parameters.AddWithValue("@nombre", persona.Nombre);
-
-                    command.ExecuteNonQuery();
-                }
-            }
-        }
-
-        public void Delete(string documento)
-        {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("DELETE FROM Personas WHERE Documento = @documento", connection))
-                {
-                    command.Parameters.AddWithValue("@documento", documento);
-
-                    command.ExecuteNonQuery();
-                }
-            }
+            throw new NotImplementedException();
         }
     }
 }
