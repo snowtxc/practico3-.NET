1597287cdacd32f0727ebc945493f88180a52065
Rodrigo Castro <snowitxc@gmail.com>
2023-09-08 20:22:54 -0300

    listo

--- DataAccessLayer/DALs/DAL_Personas_EF.cs
+++ DataAccessLayer/DALs/DAL_Personas_EF.cs
@@ -19,6 +19,8 @@ namespace DataAccessLayer.DALs
             _dbContext = dbContext;
         }
 
+        
+
         public void Delete(string documento)
         {
             Personas? personaFind = _dbContext.Personas.FirstOrDefault(p => p.Documento == documento);
